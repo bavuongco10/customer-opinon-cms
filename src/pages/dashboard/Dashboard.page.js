528a11e83993e8b7a4cc50798387f571fef9e87b
Bui Thanh Ba Vuong <bavuongco10@gmail.com>
2019-11-04 21:34:17 +0700

    feat: rearrange charts postion

--- src/pages/dashboard/Dashboard.page.js
+++ src/pages/dashboard/Dashboard.page.js
@@ -20,28 +20,28 @@ const Dashboard = () => {
     <>
       <Title>Dashboard</Title>
       <Grid container spacing={4}>
-        <Grid item lg={3} md={4} sm={6} xs={12}>
+        <Grid item lg={4} md={4} sm={6} xs={12}>
           <Widget title="Collected Provinces" upperTitle disableWidgetMenu>
             <ProvincesTile />
           </Widget>
         </Grid>
-        <Grid item lg={3} md={4} sm={6} xs={12}>
+        <Grid item lg={4} md={4} sm={6} xs={12}>
           <Widget title="Collected Hotels" upperTitle disableWidgetMenu>
             <HotelsTile />
           </Widget>
         </Grid>
-        <Grid item lg={6} md={8} sm={12} xs={12}>
+        <Grid item lg={4} md={4} sm={6} xs={12}>
           <RecordsTile />
         </Grid>
-        <Grid item xs={12}>
+        <Grid item lg={4} md={4} sm={12} xs={12}>
           <Widget title="Compare Hotels" upperTitle disableWidgetMenu>
-            <Grid container spacing={2}>
-              <Grid item lg={6} md={12} sm={12} xs={12}>
-                <DenseTable setAllAspects={setAllAspects} />
-              </Grid>
-              <Grid item lg={12} md={12} sm={12} xs={12}>
-                <ReRadarChart data={radarData} />
-              </Grid>
+            <DenseTable setAllAspects={setAllAspects} />
+          </Widget>
+        </Grid>
+        <Grid item lg={8} md={8} sm={12} xs={12}>
+          <Widget title="Aspects Radar chart" upperTitle disableWidgetMenu>
+            <Grid item lg={12} md={12} sm={12} xs={12}>
+              <ReRadarChart data={radarData} />
             </Grid>
           </Widget>
         </Grid>
